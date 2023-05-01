20ef9ac9da629bd4ac7fcaa5a68fd9e4d7624139
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2023-05-01 17:59:58 +0700

    making auto collect #2

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,14 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance = null;
+    public static GameManager Instance {
+        get{
+            if(_instance == null){
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    [Range(0f, 1f)]
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourcesConfigs;
+
+    public Transform ResourcesParent;
+    public ResourceController ResourcesPrefab;
+
+    public TMP_Text GoldInfo;
+    public TMP_Text AutoCollectInfo;
+
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+    private float _collectSound;
+
+    private double _totalGold;
     // Start is called before the first frame update
-    void Start() {}
+    void Start() {
+        AddAllResources();
+    }
 
     // Update is called once per frame
-    void Update() {}
+    void Update() {
+        _collectSound += Time.unscaledDeltaTime;
+
+        if(_collectSound >= 1f){
+            CollectPerSecond();
+            _collectSound = 0f;
+        }
+    }
+
+    private void AddAllResources() {
+        foreach (ResourceConfig config in ResourcesConfigs)
+        {
+            GameObject obj = Instantiate(ResourcesPrefab.gameObject, ResourcesParent, false);
+            ResourceController resource = obj.GetComponent<ResourceController>();
+            resource.SetConfig(config);
+            _activeResources.Add(resource);
+        }
+    }
+
+    private void CollectPerSecond(){
+        double output = 0;
+        foreach(ResourceController resource in _activeResources){
+            output += resource.GetOutput();
+        }
+
+        output *= AutoCollectPercentage;
+
+        AutoCollectInfo.text = $"Auto Collect: { output.ToString ("F1") } / second"; 
+
+        AddGold(output);
+    }
+
+    private void AddGold(double value){
+        _totalGold += value;
+        GoldInfo.text = $"Gold: { _totalGold.ToString("0")}";
+    }
 }
 
 //Fungsi System.Serializable adalah agar 
